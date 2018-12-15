9e18c30788bd988171c09f8fb4b9dbb3b06fa192
Astramentis <WesleyFLVS@gmail.com>
2018-12-14 19:04:11 -0500

    Added config file, listeners
    Created config file to host API keys, added log system to server connections., added listeners.

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://brad:brad123@ds139425.mlab.com:39425/mern_listpersonalproject'
+}
--- server.js
+++ server.js
@@ -7,3 +7,14 @@ const app = express();
 //Bodyparser Middleware
 app.use(bodyParser.json());
 
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to server
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err)); 
+
+    const port = process.env.PORT || 5000; 
+    app.listen(port, () => console.log(`Sever started on port ${port}`));
