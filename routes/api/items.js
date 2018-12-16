1a0aaa7b7036d617da8efd756ed1875a824b7938
Astramentis <WesleyFLVS@gmail.com>
2018-12-16 17:07:17 -0500

    bug fixed, added POST to api
    i hate myself

--- routes/api/items.js
+++ routes/api/items.js
@@ -11,6 +11,17 @@ router.get('/', (req, res) =>{
     Item.find()
         .sort({ date: -1 })
         .then(items => res.json(items)) 
+}); 
+
+// @route   POST api/items
+// @desc    Create a post
+// @access  Public
+router.post('/', (req, res) =>{
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save().then(item=> res.json(item));
 });
 
 module.exports = router;
